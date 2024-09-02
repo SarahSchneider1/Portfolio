0b239bac3a70bbfd5323498677f699383e40eb31
Sarah Schneider <160104575+SarahSchneider1@users.noreply.github.com>
2024-09-02 10:01:00 +0200

    fixes + add scrollbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { LandingComponentComponent } from "./landing-component/landing-component.component";
+import { AboutMeComponent } from "./about-me/about-me.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, LandingComponentComponent],
+  imports: [RouterOutlet, HeaderComponent, LandingComponentComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
