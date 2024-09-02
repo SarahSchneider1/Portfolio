fdb5c4488a9bf7fa47ed211e59eda712cb042085
Sarah Schneider <160104575+SarahSchneider1@users.noreply.github.com>
2024-09-02 08:54:36 +0200

    add landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { LandingComponentComponent } from "./landing-component/landing-component.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, LandingComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
