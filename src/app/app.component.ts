754d09b835997eca9689985e37e2fdaee1df0821
Sarah Schneider <160104575+SarahSchneider1@users.noreply.github.com>
2024-09-03 10:26:12 +0200

    animation addet frontend developer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { LandingComponentComponent } from "./landing-component/landing-component.component";
 import { AboutMeComponent } from "./about-me/about-me.component";
+import { MySkillsComponent } from './my-skills/my-skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, LandingComponentComponent, AboutMeComponent],
+  imports: [RouterOutlet, HeaderComponent, LandingComponentComponent, AboutMeComponent, MySkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
