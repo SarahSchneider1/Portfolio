8881d350c0300e60e365a6b9eccea0f0478daba9
Sarah Schneider <160104575+SarahSchneider1@users.noreply.github.com>
2024-09-04 09:12:33 +0200

    add portfolio component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from "./header/header.component";
 import { LandingComponentComponent } from "./landing-component/landing-component.component";
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { MySkillsComponent } from './my-skills/my-skills.component';
+import { PortfolioComponent } from './portfolio/portfolio/portfolio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, LandingComponentComponent, AboutMeComponent, MySkillsComponent],
+  imports: [RouterOutlet, HeaderComponent, LandingComponentComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
